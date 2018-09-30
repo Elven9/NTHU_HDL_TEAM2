011fe2eb290a4e064aa195cc460f212fb50023aa
Elven Lin <elvenlin1998@gmail.com>
2018-09-30 14:06:33 +0800

    Add Decode Testbench

---
+++ verilog/LAB2/Source File/Lab2_Team2_Decode_and_Execute_t.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module DecoderTest;
+  reg [3-1:0] code = 3'd0;
+  reg rs, rt, rd;
+  wire [16-1:0] dout;
+
+  Decode_and_Execute test (
+      .op_code(code),
+      .rs(rs),
+      .rt(rt),
+      .rd(rd)
+      );
+
+  initial begin
+    rs = 2;
+    rt = 4;
+    repeat (2 ** 3) begin
+      #1 code = code + 1'b1;
+    end
+    #1 $finish;
+  end
+endmodule
