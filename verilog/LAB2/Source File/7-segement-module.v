f32547838a6e433faf1f30b3b7cfb318705ce8d6
Elven Lin <elvenlin1998@gmail.com>
2018-09-30 18:26:56 +0800

    feat: change some logic about Port E

--- verilog/LAB2/Source File/7-segement-module.v
+++ verilog/LAB2/Source File/7-segement-module.v
@@ -16,7 +16,7 @@ module Segment_7_bit_display (Sel, Port);
     wire wire_B_1, wire_B_2, wire_B_3, wire_B_4, wire_B_5;
     wire wire_C_1, wire_C_2, wire_C_3, wire_C_4;
     wire wire_D_1, wire_D_2, wire_D_3, wire_D_4;
-    wire wire_E_1, wire_E_2, wire_E_3, wire_E_4, wire_E_5;
+    wire wire_E_1, wire_E_2, wire_E_3;
     wire wire_F_1, wire_F_2, wire_F_3, wire_F_4;
     wire wire_G_1, wire_G_2, wire_G_3;
 
@@ -24,7 +24,7 @@ module Segment_7_bit_display (Sel, Port);
     not gate_not_1(not_d, Sel[3]);
     not gate_not_2(not_c, Sel[2]);
     not gate_not_3(not_b, Sel[1]);
-    not gate_not_4(not_b, Sel[0]);
+    not gate_not_4(not_a, Sel[0]);
 
     // Port A.
     and gateA_1(wire_A_1, Sel[3], not_c, Sel[1], Sel[0]);
@@ -55,12 +55,10 @@ module Segment_7_bit_display (Sel, Port);
     or gate_D_5(Port[3], wire_D_1, wire_D_2, wire_D_3, wire_D_4);
 
     // Port E.
-    and gate_E_1(wire_E_1, Sel[3], not_c, not_b);
+    and gate_E_1(wire_E_1, not_d, Sel[0]);
     and gate_E_2(wire_E_2, not_c, not_b, Sel[0]);
-    and gate_E_3(wire_E_3, not_d, not_b, Sel[0]);
-    and gate_E_4(wire_E_4, not_d, Sel[2], not_b);
-    and gate_E_5(wire_E_5, not_d, not_c, Sel[0]);
-    or gate_E_6(Port[4], wire_E_1, wire_E_2, wire_E_3, wire_E_4, wire_E_5);
+    and gate_E_3(wire_E_3, not_d, Sel[2], not_b);
+    or gate_E_6(Port[4], wire_E_1, wire_E_2, wire_E_3);
 
     // Port F.
     and gate_F_1(wire_F_1, Sel[3], Sel[2], not_b, Sel[0]);
