9d67a15ab8e7f3548ce451266ccbcb58b7b6513a
Elven Lin <elvenlin1998@gmail.com>
2018-10-04 12:11:37 +0800

    change TB Designed

--- verilog/LAB2/Source File/Lab2_Team2_Carry_Look_Ahead_Adder_t.v
+++ verilog/LAB2/Source File/Lab2_Team2_Carry_Look_Ahead_Adder_t.v
@@ -19,15 +19,34 @@ initial begin
   b = 4;
   cin = 1;
 
+  if (sum != 8 && cout != 0) begin
+      $display("Some Error Happen In your code. Test: a = 4, b = 4 ###### Failed ######");
+  end
+  else begin
+      $display("Congratulation!! Test: a = 4, b = 4 ###### Passed ######");
+  end
+
   #3 begin
     a = 2;
     b = 5;
     cin = 0;
+    if (sum != 7 && cout != 0) begin
+        $display("Some Error Happen In your code. Test: a = 2, b = 5 ###### Failed ######");
+    end
+    else begin
+        $display("Congratulation!! Test: a = 2, b = 5 ###### Passed ######");
+    end
   end
   #3 begin
     a = 3;
     b = 1;
     cin = 1;
+    if (sum != 4 && cout != 0) begin
+        $display("Some Error Happen In your code. Test: a = 3, b = 1 ###### Failed ######");
+    end
+    else begin
+        $display("Congratulation!! Test: a = 3, b = 1 ###### Passed ######");
+    end
   end
   #1 $finish;
 end
