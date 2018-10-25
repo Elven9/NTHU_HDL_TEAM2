e28d5aa93193a536e3fe59192ede8772561ddd4d
ElvenLin <elvenlin1998@gmail.com>
2018-10-25 16:01:31 +0800

    remove weird situation.

--- verilog/LAB3/Sources files/Lab3_TeamX_Parameterized_Ping_Pong_Counter_t.v
+++ verilog/LAB3/Sources files/Lab3_TeamX_Parameterized_Ping_Pong_Counter_t.v
@@ -7,35 +7,17 @@ module Parameterized_Ping_Pong_Counter_t ();
     reg [3:0] max, min;
     wire direction;
     wire [3:0] out;
-    wire flip_out;
 
-    Lab3_TeamX_Parameterized_Ping_Pong_Counter_fpga gate1(.clk(clk),
-                                          .rst(rst_n),
+    Parameterized_Ping_Pong_Counter gate1(.clk(clk),
+                                          .rst_n(rst_n),
                                           .enable(enable),
                                           .flip(flip),
                                           .max(max),
                                           .min(min),
-                                          .out(out));
+                                          .out(out),
+                                          .direction(direction));
 
     always #(1)clk = ~clk;
-    
-    reg [3:0] delay;
-        wire debounceSignal;
-    
-        reg pulse1, pulse2;
-    
-        // Debounce;
-        always @ ( posedge clk ) begin
-            delay[3:1] <= delay[2:0];
-            delay[0] <= rst_n;
-        end
-        assign debounceSignal = delay == 4'b1111 ? 1 : 0;
-    
-        // One Pulse;
-        always @ ( posedge clk ) begin
-            pulse1 <= debounceSignal;
-            pulse2 <= (!pulse1) & debounceSignal;
-        end
 
     initial begin
         clk = 0;
@@ -44,38 +26,9 @@ module Parameterized_Ping_Pong_Counter_t ();
         min = 0;
         flip = 0;
         enable = 1;
-        #50 rst_n = 1;
-        #1 rst_n = 0;
-        #1 rst_n = 1;
-                #1 rst_n = 0;
-                #1 rst_n = 1;
-                        #1 rst_n = 0;
-                        #1 rst_n = 1;
-                                #1 rst_n = 0;
-                                #1 rst_n = 1;
-                                        #1 rst_n = 0;
-                                        #1 rst_n = 1;
-                                                #1 rst_n = 0;
-                                             #1 rst_n = 1;
-        #50 rst_n = 0;
-        #1 flip = 1;
-        #2 flip = 0;
-        #2 flip = 1;
-                #2 flip = 0;
-                #2 flip = 1;
-                        #2 flip = 0;
-                        #2 flip = 1;
-                                #2 flip = 0;
-                                #2 flip = 1;
-                                        #2 flip = 0;
-                                        #2 flip = 1;
-                                                #2 flip = 0;
-                                                #2 flip = 1;
-                                                        #2 flip = 0;
-                                                        #2 flip = 1;
-                                                        #100 flip = 0;
         
-        #1000000000 $finish;
+        
+        #100 $finish;
     end
 
 endmodule // Parameterized_Ping_Pong_Counter_t
