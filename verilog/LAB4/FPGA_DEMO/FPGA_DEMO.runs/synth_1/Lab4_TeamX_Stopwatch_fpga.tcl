f5d7a31f31004f8e32ff2b3c88dc0425b42bcda3
ElvenLin <elvenlin1998@gmail.com>
2018-11-08 15:51:41 +0800

    upload project file

--- verilog/LAB4/FPGA_DEMO/FPGA_DEMO.runs/synth_1/Lab4_TeamX_Stopwatch_fpga.tcl
+++ verilog/LAB4/FPGA_DEMO/FPGA_DEMO.runs/synth_1/Lab4_TeamX_Stopwatch_fpga.tcl
@@ -17,8 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param xicom.use_bs_reader 1
-set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -30,7 +28,7 @@ set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_output_repo {c:/Users/elven/Desktop/Hardware Design Lab/NTHU_HDL_TEAM2/verilog/LAB4/FPGA_DEMO/FPGA_DEMO.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_verilog -library xil_defaultlib {{C:/Users/elven/Desktop/Hardware Design Lab/NTHU_HDL_TEAM2/verilog/LAB4/Source Code/Lab4_TeamX_Stopwatch_fpga.v}}
+read_verilog -library xil_defaultlib {{C:/Users/elven/Desktop/Hardware Design Lab/NTHU_HDL_TEAM2/verilog/LAB4/FPGA_DEMO/FPGA_DEMO.srcs/sources_1/imports/Source Code/Lab4_Team2_Stopwatch_fpga.v}}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
