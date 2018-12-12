7f5e3463c2c14f272e2e57a3407b36bbb6a7c977
Louis <louisby0123@gmail.com>
2018-12-12 21:50:00 +0800

    modify

--- verilog/LAB6/Source Code/Lab6_Team2_Mixing_Keyboard_and_VGA.v
+++ verilog/LAB6/Source Code/Lab6_Team2_Mixing_Keyboard_and_VGA.v
@@ -38,13 +38,36 @@ module top(
   );
 
   reg right, left, up, down;
-  wire f_r, f_l, f_u, f_d;
+  wire f_r, f_l, f_u, f_d, f_v, f_h, f_p;
+
+  parameter keyH = 9'b000110011;
+  parameter keyV = 9'b000101010;
+
+  OnePulse pulse5 (.clock(clk), .signal(key_down[9'b001001101]), .signal_single_pulse(f_p));
   OnePulse pulse6 (.clock(clk), .signal(key_down[9'b001101011]), .signal_single_pulse(f_l));
   OnePulse pulse7 (.clock(clk), .signal(key_down[9'b001110100]), .signal_single_pulse(f_r));
   OnePulse pulse8 (.clock(clk), .signal(key_down[9'b001110101]), .signal_single_pulse(f_u));
   OnePulse pulse9 (.clock(clk), .signal(key_down[9'b001110010]), .signal_single_pulse(f_d));
-  
+  OnePulse pulse10 (.clock(clk), .signal(key_down[keyV]), .signal_single_pulse(f_v));
+  OnePulse pulse11 (.clock(clk), .signal(key_down[keyH]), .signal_single_pulse(f_h));
+
   assign light = {key_down[9'b001101011], key_down[9'b001110100], key_down[9'b001110101], key_down[9'b001110010]};
+
+  reg relay_v, relay_h, relay_p;
+  always @ ( posedge clk_22, posedge f_v ) begin
+    if (f_v) relay_v = 1'b1;
+    else relay_v = 1'b0;
+  end
+
+  always @ ( posedge clk_22, posedge f_h ) begin
+    if (f_h) relay_h = 1'b1;
+    else relay_h = 1'b0;
+  end
+
+  always @ ( posedge clk_22, posedge f_p ) begin
+    if (f_p) relay_p = 1'b1;
+    else relay_p = 1'b0;
+  end
 
   clock_divisor clk_wiz_0_inst(
     .clk(clk),
@@ -62,19 +85,19 @@ module top(
     .right(key_down[9'b001110100]),
     .up(key_down[9'b001110101]),
     .down(key_down[9'b001110010]),
-    .pause(key_down[9'b001001101]),
-    .v_flip(key_down[9'b000101010]),
-    .h_flip(key_down[9'b000110011])
+    .pause(relay_p),
+    .v_flip(relay_v),
+    .h_flip(relay_h)
   );
-     
- 
+
+
   blk_mem_gen_0 blk_mem_gen_0_inst(
     .clka(clk_25MHz),
     .wea(0),
     .addra(pixel_addr),
     .dina(data[11:0]),
     .douta(pixel)
-  ); 
+  );
 
   vga_controller   vga_inst(
     .pclk(clk_25MHz),
@@ -85,7 +108,7 @@ module top(
     .h_cnt(h_cnt),
     .v_cnt(v_cnt)
   );
-      
+
 endmodule
 
 module mem_addr_gen(
@@ -93,79 +116,199 @@ module mem_addr_gen(
   input rst,
   input [9:0] h_cnt,
   input [9:0] v_cnt,
-  input left, 
+  input left,
   input right,
   input up,
   input down,
   input pause,
   input v_flip,
   input h_flip,
-  output [16:0] pixel_addr
+  output reg [16:0] pixel_addr
   );
-  
+
   reg [7:0] pos_y, next_pos_y;
   reg [8:0] pos_x, next_pos_x;
-  reg [1:0] dir_x, n_dir_x, dir_y, n_dir_y;
-  reg is_v_flip, is_h_flip;
-    
-  wire [10:0] xpos, ypos;
-  assign xpos = is_h_flip ? 320 - ((h_cnt>>1) + pos_x) : (h_cnt>>1) + pos_x;
-  assign ypos = is_v_flip ? 240 - ((v_cnt>>1) + pos_y) * 320 : ((v_cnt>>1) + pos_y) * 320;
-    
-  assign pixel_addr = ((h_cnt>>1) + pos_x + ((v_cnt>>1) + pos_y) * 320)% 76800;  //640*480 --> 320*240 
-  
+  reg [1:0] dir_x, n_dir_x, dir_y, n_dir_y, keep_dir_x, keep_dir_y, n_keep_dir_x, n_keep_dir_y;
+
+  parameter WIDTH = 320;
+  parameter HEIGHT = 240;
+
+  wire [8:0] addr_h, addr_v;
+  assign addr_h = ((h_cnt>>1) + pos_x) % WIDTH;
+  assign addr_v = ((v_cnt>>1) + pos_y) % HEIGHT;
+
+  reg [1:0] flip_dir, next_flip_dir;
+  always @ ( * ) begin
+    if (v_flip) next_flip_dir[0] = !flip_dir[0];
+    else next_flip_dir[0] = flip_dir[0];
+
+    if (h_flip) next_flip_dir[1] = !flip_dir[1];
+    else next_flip_dir[1] = flip_dir[1];
+  end
+
+
+  always @ ( * ) begin
+    case (flip_dir)
+      2'b00: pixel_addr = (addr_h + (addr_v)*WIDTH) % 76800;
+      2'b01: pixel_addr = (addr_h + (HEIGHT - addr_v)*WIDTH) % 76800;
+      2'b10: pixel_addr = ((WIDTH - addr_h) + addr_v*WIDTH) % 76800;
+      2'b11: pixel_addr = ((WIDTH - addr_h) + (HEIGHT - addr_v)*WIDTH) % 76800;
+      default: pixel_addr = (addr_h + (addr_v)*WIDTH) % 76800;
+    endcase
+  end
+
   always @ (posedge clk or posedge rst) begin
     if(rst) begin
       pos_x <= 0;
       pos_y <= 0;
       dir_x <= 0;
       dir_y <= 0;
+      flip_dir <= 2'b00;
+      state <= INITIAL;
+      keep_dir_x <= 0;
+      keep_dir_y <= 0;
     end
     else begin
       pos_x <= next_pos_x;
       pos_y <= next_pos_y;
       dir_x <= n_dir_x;
       dir_y <= n_dir_y;
-    end
-  end
-  
-  always @ (posedge v_flip or posedge h_flip) begin
-    if (rst) begin
-        is_v_flip = 0;
-        is_h_flip = 0;
-    end
-    if (v_flip) begin
-        is_v_flip = !is_v_flip;
-    end
-    if (h_flip) begin
-        is_h_flip = !is_h_flip;
+      flip_dir <= next_flip_dir;
+      state <= next_state;
+      keep_dir_x <= n_keep_dir_x;
+      keep_dir_y <= n_keep_dir_y;
     end
   end
 
+  parameter INITIAL = 2'b00;
+  parameter STOP = 2'b01;
+  parameter MOVE = 2'b10;
+
+  reg [1:0] state, next_state;
+
+  always @ ( * ) begin
+    case (state)
+      INITIAL: begin
+        // Direction
+        n_dir_x = dir_x;
+        n_dir_y = dir_y;
+        if (left) begin
+          n_dir_x = 1;
+          n_keep_dir_x = 1;
+          n_dir_y = 0;
+          n_keep_dir_y = 0;
+          next_state = MOVE;
+        end
+        else if (right) begin
+          n_dir_x = 2;
+          n_keep_dir_x = 2;
+          n_dir_y = 0;
+          n_keep_dir_y = 0;
+          next_state = MOVE;
+        end
+        else if (up || pause) begin
+          n_dir_y = 1;
+          n_keep_dir_y = 1;
+          n_dir_x = 0;
+          n_keep_dir_x = 0;
+          next_state = MOVE;
+        end
+        else if (down) begin
+          n_dir_y = 2;
+          n_keep_dir_y = 2;
+          n_dir_x = 0;
+          n_keep_dir_x = 0;
+          next_state = MOVE;
+        end
+        else if (v_flip || h_flip) begin
+          n_dir_x = dir_x;
+          n_dir_y = dir_y;
+          n_keep_dir_x = keep_dir_x;
+          n_keep_dir_y = keep_dir_y;
+          next_state = MOVE;
+        end
+        else begin
+          n_dir_x = dir_x;
+          n_dir_y = dir_y;
+          n_keep_dir_x = keep_dir_x;
+          n_keep_dir_y = keep_dir_y;
+          next_state = state;
+        end
+      end
+      STOP: begin
+        if (pause) begin
+          next_state = MOVE;
+          n_dir_x = keep_dir_x;
+          n_dir_y = keep_dir_y;
+        end
+        else begin
+          next_state = state;
+          if (left) begin
+            n_keep_dir_x = 1;
+            n_keep_dir_y = 0;
+          end
+          else if (right) begin
+            n_keep_dir_x = 2;
+            n_keep_dir_y = 0;
+          end
+          else if (up) begin
+            n_keep_dir_y = 1;
+            n_keep_dir_x = 0;
+          end
+          else if (down) begin
+            n_keep_dir_y = 2;
+            n_keep_dir_x = 0;
+          end
+          else begin // here
+            n_keep_dir_x = keep_dir_x;
+            n_keep_dir_y = keep_dir_y;
+          end
+          n_dir_x = 0;
+          n_dir_y = 0;
+        end
+      end
+      MOVE: begin
+        if (pause) next_state = STOP;
+        else next_state = state;
+
+        // Direction
+        n_dir_x = dir_x;
+        n_dir_y = dir_y;
+        if (left) begin
+          n_dir_x = 1;
+          n_keep_dir_x = 1;
+          n_dir_y = 0;
+          n_keep_dir_y = 0;
+        end
+        else if (right) begin
+          n_dir_x = 2;
+          n_keep_dir_x = 2;
+          n_dir_y = 0;
+          n_keep_dir_y = 0;
+        end
+        else if (up) begin
+          n_dir_y = 1;
+          n_keep_dir_y = 1;
+          n_dir_x = 0;
+          n_keep_dir_x = 0;
+        end
+        else if (down) begin
+          n_dir_y = 2;
+          n_keep_dir_y = 2;
+          n_dir_x = 0;
+          n_keep_dir_x = 0;
+        end
+        else begin
+          n_dir_x = dir_x;
+          n_dir_y = dir_y;
+          n_keep_dir_x = keep_dir_x;
+          n_keep_dir_y = keep_dir_y;
+        end
+      end
+    endcase
+  end
+
   always @(*) begin
-    n_dir_x = dir_x;
-    n_dir_y = dir_y;
-    if (left) begin 
-        n_dir_x = 1;
-        n_dir_y = 0;
-    end
-    if (right) begin
-        n_dir_x = 2;
-        n_dir_y = 0;
-    end
-    if (up) begin
-        n_dir_y = 1;
-        n_dir_x = 0;
-    end
-    if (down) begin 
-        n_dir_y = 2;
-        n_dir_x = 0;
-    end
-    if (pause) begin
-        n_dir_x = 0;
-        n_dir_y = 0;
-    end
-    
     if (dir_x == 1) next_pos_x = pos_x == 319 ? 0 : pos_x + 1;
     else if (dir_x == 2) next_pos_x = pos_x == 0 ? 319 : pos_x - 1;
     else next_pos_x = pos_x;
@@ -173,31 +316,30 @@ module mem_addr_gen(
     if (dir_y == 1) next_pos_y = pos_y == 239 ? 0 : pos_y + 1;
     else if (dir_y == 2) next_pos_y = pos_y == 0 ? 239 : pos_y - 1;
     else next_pos_y = pos_y;
-
   end
-    
+
 endmodule
 
-module vga_controller 
+module vga_controller
   (
     input wire pclk,reset,
     output wire hsync,vsync,valid,
     output wire [9:0]h_cnt,
     output wire [9:0]v_cnt
     );
-    
+
     reg [9:0]pixel_cnt;
     reg [9:0]line_cnt;
     reg hsync_i,vsync_i;
     wire hsync_default, vsync_default;
     wire [9:0] HD, HF, HS, HB, HT, VD, VF, VS, VB, VT;
 
-   
+
     assign HD = 640;
     assign HF = 16;
     assign HS = 96;
     assign HB = 48;
-    assign HT = 800; 
+    assign HT = 800;
     assign VD = 480;
     assign VF = 10;
     assign VS = 2;
@@ -205,7 +347,7 @@ module vga_controller
     assign VT = 525;
     assign hsync_default = 1'b1;
     assign vsync_default = 1'b1;
-     
+
     always@(posedge pclk)
         if(reset)
             pixel_cnt <= 0;
@@ -220,8 +362,8 @@ module vga_controller
         else if((pixel_cnt >= (HD + HF - 1))&&(pixel_cnt < (HD + HF + HS - 1)))
                 hsync_i <= ~hsync_default;
             else
-                hsync_i <= hsync_default; 
-    
+                hsync_i <= hsync_default;
+
     always@(posedge pclk)
         if(reset)
             line_cnt <= 0;
@@ -230,22 +372,22 @@ module vga_controller
                     line_cnt <= line_cnt + 1;
                 else
                     line_cnt <= 0;
-                    
+
     always@(posedge pclk)
         if(reset)
-            vsync_i <= vsync_default; 
+            vsync_i <= vsync_default;
         else if((line_cnt >= (VD + VF - 1))&&(line_cnt < (VD + VF + VS - 1)))
-            vsync_i <= ~vsync_default; 
+            vsync_i <= ~vsync_default;
         else
-            vsync_i <= vsync_default; 
-                    
+            vsync_i <= vsync_default;
+
     assign hsync = hsync_i;
     assign vsync = vsync_i;
     assign valid = ((pixel_cnt < HD) && (line_cnt < VD));
-    
+
     assign h_cnt = (pixel_cnt < HD) ? pixel_cnt:10'd0;
     assign v_cnt = (line_cnt < VD) ? line_cnt:10'd0;
-           
+
 endmodule
 
 module clock_divisor(clk1, clk, clk22);
@@ -273,29 +415,29 @@ module KeyboardDecoder(
   input wire rst,
   input wire clk
     );
-    
+
   parameter [1:0] INIT      = 2'b00;
   parameter [1:0] WAIT_FOR_SIGNAL = 2'b01;
   parameter [1:0] GET_SIGNAL_DOWN = 2'b10;
   parameter [1:0] WAIT_RELEASE    = 2'b11;
-    
+
   parameter [7:0] IS_INIT      = 8'hAA;
   parameter [7:0] IS_EXTEND    = 8'hE0;
   parameter [7:0] IS_BREAK    = 8'hF0;
-  
+
   reg [9:0] key;    // key = {been_extend, been_break, key_in}
   reg [1:0] state;
   reg been_ready, been_extend, been_break;
-  
+
   wire [7:0] key_in;
   wire is_extend;
   wire is_break;
   wire valid;
   wire err;
-  
+
   wire [511:0] key_decode = 1 << last_change;
   assign last_change = {key[9], key[7:0]};
-  
+
   KeyboardCtrl_0 inst (
   .key_in(key_in),
   .is_extend(is_extend),
@@ -313,7 +455,7 @@ OnePulse op (
   .signal(been_ready),
   .clock(clk)
 );
-  
+
   always @ (posedge clk, posedge rst) begin
     if (rst) begin
       state <= INIT;
@@ -371,7 +513,7 @@ OnePulse op (
       endcase
     end
   end
-  
+
   always @ (posedge clk, posedge rst) begin
     if (rst) begin
       key_valid <= 1'b0;
@@ -396,7 +538,7 @@ module OnePulse (
 	input wire signal,
 	input wire clock
 	);
-	
+
 	reg signal_delay;
 
 	always @(posedge clock) begin
